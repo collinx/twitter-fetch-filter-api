70dde0b4d583c04e739a3b94224d6089fce6dce7
Shubham Mangal <shubham.mangal15@gmail.com>
2018-03-07 02:24:48 +0530

    added mongodb

--- model.js
+++ model.js
@@ -4,34 +4,32 @@ var tweetSchema = mongoose.Schema({
     
     created_at : Number,
     lang: String,
-    type:String,
-    type_id:String,
-    tweet: {
-        id:Number,
-        text: String,
-        quote_count:Number,
-        reply_count:Number,
-        retweet_count:Number,
-        favorite_count:Number,
-
-    },
+    
+    
+    id:Number,
+    text: String,
+    quote_count:Number,
+    reply_count:Number,
+    retweet_count:Number,
+    favorite_count:Number,
+    
     urls:[],
     user_mentions:[],
     hashtags:[],
+
+    country_code : String,
+     
     user : {
-             id : Number,
-             name : String,
-             screen_name : String,
-             location : String,
-             followers_count : Number,
-             following_count : Number,
-             listed_count: Number,
-             likes_count: Number,
-             tweets_count: Number,
-             joined_at: Number,
-             time_zone:String,
-         }
+        name : String,
+        screen_name : String,
+        location : String,
+        followers_count : Number,
+        following_count : Number,
+        likes_count: Number,
+        tweets_count: Number,
+        joined_at: Number,
+    }
 });
 
- 
+
 module.exports = mongoose.model('Tweet', tweetSchema);
--- routes/utils.js
+++ routes/utils.js
@@ -1,5 +1,5 @@
 var express = require('express');
- 
+
 var config = require('../config');
 var utils = require('./utils');
 var Tweet = require('../model');
@@ -10,14 +10,14 @@ var keys  =  {
   token : config.access_token_key,
   token_secret : config.access_token_secret
 };
-var Twitter = new TwitterStream(keys, false);
+
 var stream;
 
 const MAX_TWEET = 100;
 const MAX_TIME = 30;
 
 module.exports.stream_function = function(req, res, next) {
- 
+  var Twitter = new TwitterStream(keys, false);
   var keyword = req.body.track || req.query.track; 
   var max_tweet = req.body.tweet || req.query.tweet;
   var max_time = req.body.time || req.query.time;
@@ -25,7 +25,7 @@ module.exports.stream_function = function(req, res, next) {
     max_time = MAX_TIME;
   }
   var results = [];
-
+  
   var tweet_count = 0;
   var start_time = (new Date()).getTime(); 
   
@@ -33,84 +33,167 @@ module.exports.stream_function = function(req, res, next) {
   
   Twitter.on('connection success', function (uri) {
     console.log('connection success', uri);
- 
-});
-
-Twitter.on('connection aborted', function () {
-  console.log('connection aborted');
-});
-Twitter.on('connection error network', function (error) {
-  console.log('connection error network', error);
-});
-
-Twitter.on('connection error stall', function () {
-  console.log('connection error stall');
-});
-
-Twitter.on('connection error http', function (httpStatusCode) {
-  console.log('connection error http', httpStatusCode);
-});
-
-Twitter.on('connection rate limit', function (httpStatusCode) {
-  console.log('connection rate limit', httpStatusCode);
-});
-
-Twitter.on('connection error unknown', function (error) {
-  console.log('connection error unknown', error);
-    Twitter.close();
-
-});
-
-Twitter.on('data', function (obj) {
-  var data = JSON.parse(obj);
-  console.log('data', 1);
-  if(results.length == 0){
-    res.send({
-      'code' : 0,
-      'status' : "Success",
-      'message' : "Streaming Successful",
-       
-    });
-  }
-  results.push(data);
-  tweet_count++;
-
-  if(tweet_count >= max_tweet || (new Date()).getTime() >= max_time*1000 + start_time){
-    Twitter.close();
-   
-    save_data(results);
-    start_time = (new Date()).getTime();
-    tweet_count = 0;
-    results = [];
-  } 
-});
-
-Twitter.on('data keep-alive', function () {
-  console.log('data keep-alive');
-  if(tweet_count == 0 || (new Date()).getTime() >= 5*1000 + start_time){
-    Twitter.close();
-   
-    res.send({
-      'code' : 0,
-      'status' : "Failed",
-      'message' : "No Tweets Found",
-       
-    });
-  } 
-
-});
-
-Twitter.on('data error', function (error) {
-  console.log('data error', error);
-});
+    
+  });
   
- 
-  }
+  Twitter.on('connection aborted', function () {
+    console.log('connection aborted');
+  });
+  Twitter.on('connection error network', function (error) {
+    console.log('connection error network', error);
+  });
+  
+  Twitter.on('connection error stall', function () {
+    console.log('connection error stall');
+  });
+  
+  Twitter.on('connection error http', function (httpStatusCode) {
+    console.log('connection error http', httpStatusCode);
+  });
+  
+  Twitter.on('connection rate limit', function (httpStatusCode) {
+     
+    res.send({
+      'code' : httpStatusCode,
+      'status' : "Failed",
+      'message' : "connection rate limit",
+      
+    });
+  });
+  
+  Twitter.on('connection error unknown', function (error) {
+    console.log('connection error unknown', error);
+    Twitter.close();
+    
+  });
+  
+  Twitter.on('data', function (obj) {
+    var data = JSON.parse(obj);
+   
+    if(results.length == 0){
+      res.send({
+        'code' : 0,
+        'status' : "Success",
+        'message' : "Streaming Successful",
+        'data' : data,
+      });
+    }
+    results.push(data);
+    tweet_count++;
+    
+    if(tweet_count >= max_tweet || (new Date()).getTime() >= max_time*1000 + start_time){
+      Twitter.close();
+      
+      save_data(results);
+      start_time = (new Date()).getTime();
+      tweet_count = 0;
+      results = [];
+    } 
+  });
+  
+  Twitter.on('data keep-alive', function () {
+    console.log('data keep-alive');
+    if(tweet_count == 0 || (new Date()).getTime() >= 5*1000 + start_time){
+      Twitter.close();
+      
+      res.send({
+        'code' : 0,
+        'status' : "Success",
+        'message' : "No Tweets Found",
+        
+      });
+    } 
+    
+  });
+  
+  Twitter.on('data error', function (error) {
+    console.log('data error', error);
+  });
+  
+  
+}
 
 var save_data = function(results) {
+  console.log(results.length);
+  
+  results.forEach(data => {
+    var tweet = new Tweet();
+    var head;
+    if(data.retweeted_status !=undefined){
+      head =  data.retweeted_status;
+    }else{
+      head =  data;
+    }
+
+     
+      tweet.created_at = (new Date(head.created_at)).getTime();
+      tweet.lang = head.lang;
+      tweet.id = data.id;
+
+
+      if(head.truncated == true){
+        tweet.text = head.extended_tweet.full_text;
+      }else {
+        tweet.text = head.text;
+      }
+
+      tweet.quote_count = head.quote_count;
+      tweet.reply_count = head.reply_count;
+      tweet.retweet_count = head.retweet_count;
+      tweet.favorite_count = head.favorite_count;
+
+      tweet.hashtags = [];
+      head.entities.hashtags.forEach(hash => {
+        tweet.hashtags.push(hash.text);
+      })
+      tweet.urls = [];
+      head.entities.urls.forEach(url => {
+        tweet.urls.push(url.url);
+      })
+
+      tweet.user_mentions = [];
+      head.entities.user_mentions.forEach(user => {
+        tweet.user_mentions.push(user.screen_name);
+      })
+      
+      if(head.place != undefined){
+        tweet.country_code = head.place.country_code;
+      }
+
+      tweet.user.name = head.user.name;
+      tweet.user.screen_name = head.user.screen_name;
+      tweet.user.location = head.user.location;
+      tweet.user.followers_count = head.user.followers_count;
+      tweet.user.following_count = head.user.friends_count;
+      tweet.user.likes_count = head.user.favourites_count;
+      tweet.user.tweets_count = head.user.statuses_count;
+      tweet.user.joined_at = (new Date(head.user.created_at)).getTime() ;
   
 
-    var tweet = new Tweet();
+    
+      
+     
+      tweet.save(function(err){
+
+        if (err) {
+          console.log(err);
+          return;
+      }
+      else {
+          
+          return;
+      }
+      });
+
+    
+    
+    
+   console.log(tweet);
+    
+  })  
   
-    console.log(results.length);
-    }
+  
+  
+  
+  
+}
