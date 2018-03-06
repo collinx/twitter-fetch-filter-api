5d4a7316e36ff1561f38694cb8ff82edae5d7497
Shubham Mangal <shubham.mangal15@gmail.com>
2018-03-06 20:18:46 +0530

    Streaming implemented with time and tweets

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var Twitter = require('twitter');
-var config = require('../config');
-var client = new Twitter({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token_key,
-  access_token_secret: config.access_token_secret
-});
-
+var utils = require('./utils');
  
-var stream_function = function(req, res, next) {
  
-let keyword = req.body.track || req.query.track; 
-
-// You can also get the stream in a callback if you prefer. 
-client.stream('statuses/filter', {track: keyword}, function(stream) {
-  let results=[];
-  let count=0;
-  stream.on('data', function(event) {
-    results.push(event); 
-    count++;
-    
-    if(count >= 5){
-      stream.destroy();
-      console.log("Streaming Completed");
-      res.send({'data':results});
-    } 
-
-  });
 
 
+router.get('/twitter/stream',utils.stream_function);
 
- 
-  stream.on('error', function(error) {
-    throw error;
-  });
-});
-}
-
-router.get('/twitter/stream',stream_function);
-
-router.post('/twitter/stream', stream_function);
+router.post('/twitter/stream', utils.stream_function);
 
 
 
--- routes/utils.js
+++ routes/utils.js
@@ -1,9 +1,63 @@
 var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var Twitter = require('twitter');
+var config = require('../config');
+var utils = require('./utils');
+var client = new Twitter({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token_key: config.access_token_key,
+  access_token_secret: config.access_token_secret
 });
 
-module.exports = router;
+const MAX_TWEET = 100;
+const MAX_TIME = 30;
+
+module.exports.stream_function = function(req, res, next) {
+ 
+  let keyword = req.body.track || req.query.track; 
+  let max_tweet = req.body.tweet || req.query.tweet || MAX_TWEET;
+  let max_time = req.body.time || req.query.time || MAX_TIME;
+  let tweet_count = 0;
+  let start_time = (new Date()).getTime(); 
+  // You can also get the stream in a callback if you prefer. 
+  client.stream('statuses/filter', {track: keyword}, function(stream) {
+    let results=[];
+    let count=0;
+    stream.on('data', function(event) {
+      results.push(event); 
+      tweet_count++;
+      console.log(tweet_count, start_time, (new Date()).getTime() );
+      if(tweet_count >= max_tweet){
+        stream.destroy();
+       
+        res.send({
+          'code' : 0,
+          'status' : "Success",
+          'message' : "Streaming Completed with "+ tweet_count +" Tweets"
+        });
+      } 
+
+      if( (new Date()).getTime() >= max_time*1000 + start_time){
+        stream.destroy();
+       
+        res.send({
+          'code' : 0,
+          'status' : "Success",
+          'message' : "Streaming Completed with"+ tweet_count +"Tweets"
+        });
+      } 
+      
+  
+    });
+    stream.on('error', function(error) {
+      
+      res.send({
+        'code' : -1,
+        'status' : "Failed",
+        'message' : "" + error.toString() ,
+      });
+    });
+  });
+  }
+
+
